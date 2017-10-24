5303466c9ff7fddb712d6dd30e2b7f9f08c9fd1e
DragonEmperorG <372972084@qq.com>
2017-10-24 20:00:09 +0800

    Add load_scene.py file.

--- ai_challenger_scene/ai_challenger_scene.py
+++ ai_challenger_scene/ai_challenger_scene.py
@@ -1,27 +1,37 @@
 import os
 import pickle
 import numpy as np
+import six
 
 from PIL import Image
 
 
 DATA_URL_SCENE_TRAIN = "ai_challenger_scene_train_20170904"
 DATA_URL_SCENE_VALIDATION = "ai_challenger_scene_validation_20170904"
-DATA_URL_SCENE_TRAIN = "ai_challenger_scene_test_20170904"
+DATA_URL_SCENE_TEST = "ai_challenger_scene_test_20170904"
 
 
 __all__ = ['SceneTrain']
 
-def read_scene(filenames, scene_classnum):
+
+def read_scene(filenames, scene_classnum, train_or_validation_or_test):
+    assert scene_classnum == 80
     ret = []
     fo = open(filenames, 'rb')
-    dic = pickle.load(fo, encoding='bytes')
+    if six.PY3:
+        dic = pickle.load(fo, encoding='bytes')
+    else:
+        dic = pickle.load(fo)
     data = dic[b'data']
     label = dic[b'labels']
-    
-    IMG_NUM = 53879
+    if train_or_validation_or_test == 'train':
+        img_num = 53879
+    elif train_or_validation_or_test == 'validation':
+        img_num = 7120
+    else:
+        img_num = 7040
     fo.close()
-    for k in range(IMG_NUM):
+    for k in range(img_num):
         img = data[k].reshape(3, 32, 32)
         img = np.transpose(img, [1, 2, 0])
         new_im = Image.fromarray(img)
@@ -29,11 +39,9 @@ def read_scene(filenames, scene_classnum):
         ret.append([img, label[k]])
     return ret
 
-reader = read_cifar(r"d:\pickle_test\ai_challenger_scene_train_20170904")
-
 
 class SceneBase:
-    def __init__(self, train_or_validation_or_test, shuffle=True, dir=None, scene_classnum=80):
+    def __init__(self, train_or_validation_or_test, shuffle=True, directory=None, scene_classnum=80):
         assert train_or_validation_or_test in ['train', 'validation', 'test']
         assert scene_classnum == 80
         self.scene_classnum = scene_classnum
@@ -44,8 +52,8 @@ class SceneBase:
         else:
             self.fs = os.path.join('..', DATA_URL_SCENE_TRAIN)
         self.train_or_validation_or_test = train_or_validation_or_test
-        self.data = read_scene(self.fs, scene_classnum)
-        self.dir = dir
+        self.data = read_scene(self.fs, scene_classnum, train_or_validation_or_test)
+        self.directory = directory
         self.shuffle = shuffle
 
     def size(self):
@@ -56,4 +64,17 @@ class SceneBase:
         else:
             data_size = 7040
         return data_size
-    
+
+    def get_data(self):
+        idxs = np.arange(len(self.data))
+        for k in idxs:
+            yield self.data[k]
+
+    def get_per_pixel_mean(self):
+        """
+        return a mean image of all (train and validation and test) images of size 32x32x3
+        """
+        all_imgs = [x[0] for x in read_cifar(fnames, self.cifar_classnum)]
+        arr = np.array(all_imgs, dtype='float32')
+        mean = np.mean(arr, axis=0)
+        return mean
---
+++ ai_challenger_scene/load_scene.py
@@ -0,0 +1,92 @@
+# -*- coding: utf-8 -*-
+import sys
+import cv2
+import pickle
+import numpy as np
+
+from keras import backend as K
+from keras.utils import np_utils
+
+DATA_URL_SCENE_TRAIN = "D:\\QlabChallengerRepo\\dataset\\ai_challenger_scene_train_direct_resize"
+DATA_URL_SCENE_VALIDATION = "D:\\QlabChallengerRepo\\dataset\\ai_challenger_scene_validation"
+DATA_URL_SCENE_TEST = "D:\\QlabChallengerRepo\\dataset\\ai_challenger_scene_test"
+
+nb_train_samples = 53879 # 53879 training samples
+nb_valid_samples = 7120 # 100 validation samples
+num_classes = 80
+
+
+def load_batch(fpath, label_key='labels'):
+    """Internal utility for parsing AI Challenger Scene data.
+
+    # Arguments
+        fpath: path the file to parse.
+        label_key: key for label data in the retrieve
+            dictionary.
+
+    # Returns
+        A tuple `(data, labels)`.
+    """
+    f = open(fpath, 'rb')
+    if sys.version_info < (3,):
+        d = pickle.load(f)
+    else:
+        d = pickle.load(f, encoding='bytes')
+        # decode utf8
+        d_decoded = {}
+        for k, v in d.items():
+            d_decoded[k.decode('utf8')] = v
+        d = d_decoded
+    f.close()
+    data = d['data']
+    labels = d[label_key]
+
+    data = data.reshape(data.shape[0], 3, 32, 32)
+    return data, labels
+
+
+def load_data():
+    """Loads AI Challenger Scene dataset.
+
+    # Returns
+        Tuple of Numpy arrays: `(x_train, y_train), (x_test, y_test)`.
+    """
+    num_train_samples = 53879
+
+    x_train = np.zeros((num_train_samples, 3, 32, 32), dtype='uint8')
+    y_train = np.zeros((num_train_samples,), dtype='uint8')
+
+    x_train, y_train = load_batch(DATA_URL_SCENE_TRAIN)
+    x_test, y_test = load_batch(DATA_URL_SCENE_VALIDATION)
+
+    y_train = np.reshape(y_train, (len(y_train), 1))
+    y_test = np.reshape(y_test, (len(y_test), 1))
+
+    if K.image_data_format() == 'channels_last':
+        x_train = x_train.transpose(0, 2, 3, 1)
+        x_test = x_test.transpose(0, 2, 3, 1)
+
+    return (x_train, y_train), (x_test, y_test)
+
+
+def load_scene_data(img_rows, img_cols):
+
+    # Load cifar10 training and validation sets
+    (X_train, Y_train), (X_valid, Y_valid) = load_data()
+
+    # Resize trainging images
+    if K.image_dim_ordering() == 'th':
+        X_train = np.array([cv2.resize(img.transpose(1,2,0), (img_rows,img_cols)).transpose(2,0,1) for img in X_train[:nb_train_samples,:,:,:]])
+        X_valid = np.array([cv2.resize(img.transpose(1,2,0), (img_rows,img_cols)).transpose(2,0,1) for img in X_valid[:nb_valid_samples,:,:,:]])
+    else:
+        X_train = np.array([cv2.resize(img, (img_rows,img_cols)) for img in X_train[:nb_train_samples,:,:,:]])
+        X_valid = np.array([cv2.resize(img, (img_rows,img_cols)) for img in X_valid[:nb_valid_samples,:,:,:]])
+
+    # Transform targets to keras compatible format
+    Y_train = np_utils.to_categorical(Y_train[:nb_train_samples], num_classes)
+    Y_valid = np_utils.to_categorical(Y_valid[:nb_valid_samples], num_classes)
+
+    return X_train, Y_train, X_valid, Y_valid
+
+
+data_set = load_scene_data(32, 32)
